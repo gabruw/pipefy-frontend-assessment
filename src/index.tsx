2ca51a220c8ef9d466a689f32553b70a61cbd193
gabruw <gabrielmarques.mg@gmail.com>
2021-10-07 11:38:36 -0300

    refact: Change client in app index

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,21 @@
+//#region Imports
+
+import { ApolloProvider } from '@apollo/client';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import client from './services/client';
 
-const client = new ApolloClient({
-  uri: process.env.REACT_APP_PIPEFY_API_URL || '',
-  cache: new InMemoryCache(),
-  headers: {
-    authorization: `Bearer ${process.env.REACT_APP_PIPEFY_API_TOKEN || ''}`,
-  },
-  credentials: 'same-origin',
-});
+//#endregion
 
 ReactDOM.render(
-  <React.StrictMode>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <ApolloProvider client={client}>
+            <App />
+        </ApolloProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();

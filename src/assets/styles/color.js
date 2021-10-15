ee312f9b088467de6a611621cc007e9ec5439b0e
gabruw <gabrielmarques.mg@gmail.com>
2021-10-14 21:29:12 -0300

    feat: Logo, color pallets and changes in global style

---
+++ src/assets/styles/color.js
@@ -0,0 +1,23 @@
+const COLOR = {
+    PRIMARY: {
+        DARK: '#4A4A4A',
+        LIGHT: '#A9A9A9',
+        MEDIUM: '#22252b'
+    },
+    SECONDARY: {
+        DARK: '#4D616E',
+        LIGHT: '#ffffff',
+        MEDIUM: '#D2E0EC'
+    },
+    INFO: {
+        LIGHT: '#EAF1FE',
+        MEDIUM: '#266AF6'
+    },
+    NEGATIVE: {
+        DARK: '#B50404',
+        LIGHT: '#DB1A1A',
+        MEDIUM: '#cF0F0F'
+    }
+};
+
+export default COLOR;

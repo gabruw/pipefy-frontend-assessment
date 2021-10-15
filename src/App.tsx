0507811d103ca789d53ad6a7789e31695a6b920a
gabruw <gabrielmarques.mg@gmail.com>
2021-10-14 21:38:17 -0300

    feat: Change theme in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 //#region Imports
 
 import { ApolloProvider } from '@apollo/client';
+import { ThemeProvider } from '@material-ui/styles';
 import 'assets/styles/global.css';
 import Loader from 'components/Loader';
 import Snackbar from 'components/Snackbar';
+import useTheme from 'hooks/useTheme';
 import React, { FC } from 'react';
 import Router from 'routes/Router';
 import useClient from 'services/useClient';
@@ -12,13 +14,16 @@ import useClient from 'services/useClient';
 
 const App: FC = () => {
     const client = useClient();
+    const theme = useTheme();
 
     return (
         <ApolloProvider client={client}>
-            <Router />
+            <ThemeProvider theme={theme}>
+                <Router />
 
-            <Loader />
-            <Snackbar />
+                <Loader />
+                <Snackbar />
+            </ThemeProvider>
         </ApolloProvider>
     );
 };
